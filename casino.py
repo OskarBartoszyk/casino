86b487a13b674fd6eca65feb59a564b2ac699ae1
Oskar Bartoszyk <oskarbartoszyk@gmail.com>
2023-05-27 20:31:00 +0200

    adding global variables/learning new stuff

--- casino.py
+++ casino.py
@@ -4,10 +4,9 @@ money = 1000
 win_price = 100
 loss_price = 20
 new_value = 0
-
-
-
 def spin():
+	global money
+	global new_value
 	number1 = random.randint(1,3)
 	number2 = random.randint(1,3)
 	number3 = random.randint(1,3)
@@ -16,20 +15,20 @@ def spin():
 		print(spin)
 		print(f"You won {win_price} $")
 		new_value = money + win_price
+		print( new_value)
 	else:
 		spin = [number1,number2,number3]
 		print(spin)
 		print(f"You lost {loss_price} $")
 		new_value = money - loss_price
+		print( new_value)
 spin()
-def check():
-	print(new_value)
-while money >= 20:
-	inter1 = input("Do you want to spin again? (type SPIN) If you want to check your balance type CHECK: ")
-	inter1 = inter1.lower()
-	if inter1 == "spin":
+while True:
+	inter = input("Do you want to spin again? (type SPIN) If you want to check your balance type CHECK: ")
+	inter = inter.lower()
+	if inter == "spin":
 		spin()
-	elif inter1 == "check":
-		check()
+	elif inter == "check":
+		pass
 	else:
 		break
